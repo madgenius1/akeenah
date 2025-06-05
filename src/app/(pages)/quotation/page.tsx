9e6702d92fb866721b7e06b8a024da304fdb0fd7
MadGenius <nickjuma@outlook.com>
2025-06-05 12:04:28 +0300

    Modified a lot of pages

--- src/app/(pages)/quotation/page.tsx
+++ src/app/(pages)/quotation/page.tsx
@@ -1,12 +1,12 @@
 
 
-export default function Home() {
+export default function Page() {
     return(
         <main>
             <div className="min-h-screen bg-white">
                 <div className="flex items-center justify-center">
                     <h2 className="text-xl font-semibold text-gray-900">
-                        Landing Page
+                        Quotation
                     </h2>
                 </div>
             </div>
--- src/app/(pages)/services/page.tsx
+++ src/app/(pages)/services/page.tsx
@@ -1,12 +1,13 @@
 
 
-export default function Home() {
+
+export default function Page() {
     return(
         <main>
             <div className="min-h-screen bg-white">
                 <div className="flex items-center justify-center">
                     <h2 className="text-xl font-semibold text-gray-900">
-                        Landing Page
+                        Services
                     </h2>
                 </div>
             </div>
