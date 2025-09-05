0e87abd490d68c9ac77224a5d78f500d4861f154
MadGenius <nickjuma@outlook.com>
2025-09-05 18:04:26 +0300

    Reworked and restarted

--- src/file/components/common/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="p-6 text-center bg-gray-100 border-t mt-10">
-      <p className="text-sm text-gray-600">© {new Date().getFullYear()} Akeenah Group. All rights reserved.</p>
-    </footer>
-  );
-}
--- src/file/lib/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_CMS_URL || "http://localhost:3001/api",
-});
-
-export default api;
