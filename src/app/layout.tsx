643b7e5ad521c056a4bd843e5fdd9298ca7d481a
MadGenius <nickjuma@outlook.com>
2025-09-06 16:15:54 +0300

    Modified layout without Header.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,22 @@
 import "./globals.css";
 import { ReactNode } from "react";
+import Footer from "./components/common/Footer";
 
 export const metadata = {
   title: "Akeenah Group | Home",
   description: "Africa's Leading Industrial Procurement and Supply Company",
 };
 
+
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex flex-col bg-gray-50 text-gray-900">
-        {children}
+      <body className="min-h-screen flex flex-col">
+        <main className="flex-1">{children}</main>
+        <Footer />
       </body>
     </html>
   );
 }
+
